3dd3b719a8e12f9f85aacff8462e1db58b829096
DESKTOP-QGM21HP\刘虹志 <1363996082@qq.com>
2017-07-17 22:14:17 +0800

    修改了存放img的路径

--- router/router.js
+++ router/router.js
@@ -5,7 +5,7 @@ let db = require('../db/db.js');
 module.exports = (app) => {
     app.post('/updateimg/:image', (req, res) => {
         let img = req.body.img.split(',')[1];
-        fs.writeFile('F:/myblog/img/' + req.body.name, img, 'base64', (err) => {
+        fs.writeFile('./img/' + req.body.name, img, 'base64', (err) => {
             if (err) {
                 console.log(err);
                 res.sendStatus(400);
